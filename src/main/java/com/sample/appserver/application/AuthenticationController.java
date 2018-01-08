ae7cb2ad7b16b57cdd129da0d559262853edbad6
Satoshi Hikida <s.hikida@hotmail.co.jp>
2018-01-08 13:21:04 +0900

    validation with json error object

--- src/main/java/com/sample/appserver/application/AuthenticationController.java
+++ src/main/java/com/sample/appserver/application/AuthenticationController.java
@@ -1,6 +1,11 @@
 package com.sample.appserver.application;
 
-import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by satoshi on 2017/11/07.
@@ -8,9 +13,11 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class AuthenticationController {
 
-	@PostMapping(path = "/authentication", consumes = "application/json")
-	public AccessToken authentication(@RequestBody UserCredential credential) {
-		AccessToken at = new AccessToken("abcdefg", 3600L);
-		return at;
-	}
+  private static final Logger LOGGER = LoggerFactory.getLogger(AuthenticationController.class);
+
+  @PostMapping(path = "/authentication", consumes = "application/json")
+  public AccessToken authentication(@Validated @RequestBody UserCredential credential) {
+    AccessToken at = new AccessToken("abcdefg" + credential.userId, 3600L);
+    return at;
+  }
 }
---
+++ src/main/java/com/sample/appserver/application/ValidationExceptionHandler.java
@@ -0,0 +1,57 @@
+package com.sample.appserver.application;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+/**
+ * Created by satoshi on 2018/01/08.
+ */
+@ControllerAdvice
+public class ValidationExceptionHandler extends ResponseEntityExceptionHandler {
+
+  @Override
+  protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+      HttpHeaders headers, HttpStatus status, WebRequest request) {
+    BindingResult bindingResult = ex.getBindingResult();
+
+    List<ApiFieldError> fieldErrors = bindingResult.getFieldErrors().stream().map(fe -> new ApiFieldError(
+        fe.getField(),
+        fe.getCode(),
+        fe.getRejectedValue(),
+        fe.getDefaultMessage()
+    )).collect(Collectors.toList());
+
+    ApiErrorsView errorsView = new ApiErrorsView(fieldErrors);
+    return new ResponseEntity<>(errorsView, HttpStatus.UNPROCESSABLE_ENTITY);
+  }
+
+  public static class ApiErrorsView {
+    public List<ApiFieldError> fieldErrors;
+    public ApiErrorsView(List<ApiFieldError> fieldErrors) {
+      this.fieldErrors = fieldErrors;
+    }
+  }
+
+  public static class ApiFieldError {
+    public String field;
+    public String code;
+    public Object rejectedValue;
+    public String message;
+
+    public ApiFieldError(String field, String code, Object rejectedValue, String message) {
+      this.field = field;
+      this.code = code;
+      this.rejectedValue = rejectedValue;
+      this.message = message;
+    }
+  }
+
+}
